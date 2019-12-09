769f302a2af89f29e21bd8c80a072cd067afe408
unknown <ronaldcontact2019@gmail.com>
2019-12-09 15:30:11 -0300

    importando styleds-components

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App styled-components';
+import App from './App Estilização com StyleSheet';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
