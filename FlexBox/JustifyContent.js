a142412ecbf9257cce6cb269f86f88633e1f928a
unknown <ronaldcontact2019@gmail.com>
2019-12-10 13:15:22 -0300

    Pequenas alterações - Justify Content

--- FlexBox/JustifyContent.js
+++ FlexBox/JustifyContent.js
@@ -1,22 +1,25 @@
+import React from 'react';
 import { View } from 'react-native';
 
-class JustifyContent extends Component {
-    render(){
-        return(
-            // O atributo 'space-between' faz que os herdeiros compartilhem
-            // igualmento os espaços disponivel pera view principal.
-            // caso não seja declarado explicitamente nenhum view principal
-            // ele utiliza todos o espaço disponivel do dispositivo.
-            
-            <View style={{
-                flex:1,
-                justifyContent:'space-between',
-            }}>
-                <View style={{width:50, height: 50, backgroundColor:'powderblue'}} />
-            </View>
-        );
-    }
+function JustifyContent() {
+    return(
+        // O atributo 'space-between' faz que os herdeiros compartilhem
+        // igualmento o espaçamento disponivel pela view principal.
+        // caso não seja declarado explicitamente nenhum view principal
+        // ele utiliza todos o espaço disponivel do dispositivo.
+        <View style={{flex:1, backgroundColor:'#3A125D'}}>   
+        <View style={{
+            flex:2, 
+            justifyContent:'space-between',
+            flexDirection:'column',
+        }}>
+            <View style={{width:50, height: 50, borderRadius:10, backgroundColor:'powderblue'}} />
+            <View style={{width:50, height:50, borderRadius:15, backgroundColor:'skyblue'}} />
+            <View style={{width:50, height:50, borderRadius:15, backgroundColor:'steelblue'}} />
+            <View style={{width:50, height:50, borderRadius:15, backgroundColor:'red'}} />
+        </View>  
+        </View> 
+    )
 };
-
 
 export default JustifyContent;
