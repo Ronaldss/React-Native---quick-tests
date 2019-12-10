699323152b1d797cf5d013edf128e010746228a8
unknown <ronaldcontact2019@gmail.com>
2019-12-10 11:38:04 -0300

    Ajuste de cor - FlexBox

--- FlexBox/Flex2.js
+++ FlexBox/Flex2.js
@@ -1,6 +1,6 @@
 // Aprendendo a usar o Flex - Flex2
 
-import React, { Components } from 'react';
+import React, { Component } from 'react';
 import { View } from 'react-native';
 
 export default class FlexBasico2 extends Component {
@@ -11,7 +11,7 @@ export default class FlexBasico2 extends Component {
       <View style={{flexDirection:'row', width:150, backgroundColor:'red'}}>
         <View style={{flex:1, height: 50, backgroundColor:'powderblue'}} />
         <View style={{flex:2, heigth:50, backgroundColor: 'skyblue'}} />
-        <View style={{flex:3, heigth: 50, backgroundColor:'steelblueadb devices'}} />
+        <View style={{flex:3, heigth: 50, backgroundColor:'steelblue'}} />
       </View>
     );
   }
